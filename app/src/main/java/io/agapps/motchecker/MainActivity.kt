20a951c3433623e3ffc911fcdd095f20deaaaf5a
Alex Green <alexandermichaelgreen@gmail.com>
2022-04-30 23:53:52 +0100

    Set transparent status bar

--- app/src/main/java/io/agapps/motchecker/MainActivity.kt
+++ app/src/main/java/io/agapps/motchecker/MainActivity.kt
@@ -9,12 +9,14 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import io.agapps.motchecker.search.screens.SearchScreen
 import io.agapps.motchecker.ui.theme.MOTCheckerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             MOTCheckerTheme {
                 Surface(
