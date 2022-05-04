b5c0424dcb96d3ae186c69763c54f82b7ec97d97
Alex Green <alexandermichaelgreen@gmail.com>
2022-05-04 21:43:09 +0100

    Add bottom bar to home screen

---
+++ app/src/main/java/io/agapps/motchecker/ui/components/AppBottomBar.kt
@@ -0,0 +1,38 @@
+package io.agapps.motchecker.ui.components
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material.BottomAppBar
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import io.agapps.motchecker.R
+import io.agapps.motchecker.ui.theme.SurfaceGrey
+
+@Composable
+fun AppBottomBar(
+    modifier: Modifier,
+    endIcon: @Composable (() -> Unit)? = null,
+) {
+    BottomAppBar(
+        modifier = modifier,
+        backgroundColor = SurfaceGrey,
+        cutoutShape = MaterialTheme.shapes.small.copy(CornerSize(percent = 50)),
+        elevation = 0.dp
+    ) {
+        IconButton(
+            onClick = {
+                // TODO: Show menu bottomsheet
+            }) {
+            Icon(Icons.Filled.Menu, contentDescription = stringResource(id = R.string.menu))
+        }
+        Spacer(Modifier.weight(1f, true))
+        endIcon?.invoke()
+    }
+}
